9e3cfce1ad820f84807f66e53e4d20120b15606e
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2023-02-09 22:08:49 +0530

    More Code

--- A1/main.cpp
+++ A1/main.cpp
@@ -13,17 +13,20 @@
 
 using namespace std;
 
+template <typename T>
 struct Chunk {
     int x, y;
-    
-    vector<vector<int>> d_og;
-    vector<vector<int>>*d = &d_og;
 
-    Chunk(const Chunk& other):x(other.x), y(other.y), d(other.d){}
+    T* d;
 
-    Chunk(int x = 0, int  y = 0, int m = 1) : x(x), y(y), d_og(m, std::vector<int>(m,0)), d(&d_og) {}
 
-    bool operator<(const Chunk& other) const {
+    Chunk(const Chunk<T>& other):x(other.x), y(other.y), d(other.d){}
+
+    Chunk(int x = 0, int  y = 0, int m = 1) : x(x), y(y) {
+        d = (T*)std::malloc(sizeof(T)*m*m);
+    }
+
+    bool operator<(const Chunk<T>& other) const {
         if (x != other.x) {
             return x < other.x;
         } else {
@@ -31,71 +34,125 @@ struct Chunk {
         }
     }
 
-    Chunk operator*(const Chunk& other) const {
-        assert(y == other.x);
-        assert(d->size() == other.d->size());
+    void clr(int m){
+        for (int i = 0; i < m*m; ++i) {
+            d[i] = 0;
+        }
+    }
 
-        int m = d->size();
-        Chunk result(x, other.y, m);
-        for (int i = 0; i < m; i++) {
-            for (int k = 0; k < m; k++) {
-                for (int j = 0; j < m; j++) {
-                    (*(result.d))[i][j] = Outer((*(result.d))[i][j], Inner((*d)[i][k], (*(other.d))[k][j]));
+
+};
+
+void mult_add(int chunk_size,Chunk<int> & result, Chunk<unsigned char > & r1,Chunk<unsigned char > & r2 ){
+    assert(result.x == r1.x);
+    assert(result.y == r2.y);
+    assert(r1.y == r2.x);
+    int i,j,k;
+
+    if(r1.x <= r1.y && r2.x <= r2.y){
+        for (i = 0; i < chunk_size; ++i) {
+            for (j = 0; j < chunk_size; ++j) {
+                for (k = 0; k < chunk_size; ++k) {
+                        result.d[i*chunk_size + k] += r1.d[i*chunk_size + j] * r2.d[j*chunk_size + k];
+                    }
+                }
+            }
+
+    }else if(r1.y <= r1.x && r2.x <= r2.y){
+
+        for (j = 0; j < chunk_size; ++j) {
+            for (i = 0; i < chunk_size; ++i) {
+                for (k = 0; k < chunk_size; ++k) {
+                    result.d[i*chunk_size + k] += r1.d[j*chunk_size + i] * r2.d[j*chunk_size + k];
                 }
             }
         }
-        return result;
-    }
 
-    Chunk operator+(const Chunk& other) const {
-        assert(d->size() == other.d->size());
-        assert(x == other.x && y == other.y);
+    }else if(r1.x <= r1.y && r2.y <= r2.x){
+        for (i = 0; i < chunk_size; ++i) {
+            for (k = 0; k < chunk_size; ++k) {
+                for (j = 0; j < chunk_size; ++j) {
 
-        int m = d->size();
-        Chunk result(x, y, m);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                (*(result.d))[i][j] = Outer((*d)[i][j], (*(other.d))[i][j]);
+                    result.d[i*chunk_size + k] += r1.d[i*chunk_size + j] * r2.d[k*chunk_size + j];
+                }
             }
         }
-        return result;
+    }else{
+        cout << "Hmmmmmm";
     }
 
-    Chunk& operator+=(const Chunk& other) {
-        assert(d->size() == other.d->size());
-        assert(x == other.x && y == other.y);
+//    int (*array_ptr)[chunk_size] = (int(*)[chunk_size])result.d;
 
-        int m = d->size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < m; j++) {
-                (*d)[i][j] = Outer((*d)[i][j], (*(other.d))[i][j]);
-            }
-        }
-        return *this;
-    }
+//    int (arr)[chunk_size][chunk_size] = *reinterpret_cast<int (*)[chunk_size][chunk_size]>(result.d);
 
-};
+
+
+
+//
+//    if (r1.x > r1.y) {
+//        if (r2.x > r2.y) {
+//            for (i = 0; i < chunk_size; ++i) {
+//                for (k = 0; k < chunk_size; ++k) {
+//                    for (j = 0; j < chunk_size; ++j) {
+//                        result.d[k][i] += r1.d[j][i] * r2.d[k][j];
+//                    }
+//                }
+//            }
+//        } else {
+//            for (i = 0; i < chunk_size; ++i) {
+//                for (k = 0; k < chunk_size; ++k) {
+//                    for (j = 0; j < chunk_size; ++j) {
+//                        result.d[k][i] += r1.d[j][i] * r2.d[j][k];
+//                    }
+//                }
+//            }
+//        }
+//    } else {
+//        if (r2.x > r2.y) {
+//            for (i = 0; i < chunk_size; ++i) {
+//                for (k = 0; k < chunk_size; ++k) {
+//                    for (j = 0; j < chunk_size; ++j) {
+//                        result.d[k][i] += r1.d[i][j] * r2.d[k][j];
+//                    }
+//                }
+//            }
+//        } else {
+//            for (i = 0; i < chunk_size; ++i) {
+//                for (k = 0; k < chunk_size; ++k) {
+//                    for (j = 0; j < chunk_size; ++j) {
+//                        result.d[k][i] += r1.d[i][j] * r2.d[j][k];
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+
+
+
+
+}
 
 
 
 int main(int argc, char *argv[])
 {
 
-    int n,m,k;
+    int n,m,chunk_count;
 
     ifstream input(argv[1], ios::binary);
 
 
     input.read((char*)&n, 4);
     input.read((char*)&m, 4);
-    input.read((char*)&k, 4);
+    input.read((char*)&chunk_count, 4);
 
-    vector<Chunk> chunks(2*k);
+    vector<Chunk<unsigned char>> chunks(2*chunk_count);
     set<int> indices;
 //    map<int,int> indices;
 //    vector<Chunk> chunks_by_col(k);
 
-    for (int i = 0; i < k; i++) {
+    for (int i = 0; i < chunk_count; i++) {
         int x, y;
         input.read((char*)&x, 4);
         input.read((char*)&y, 4);
@@ -103,19 +160,23 @@ int main(int argc, char *argv[])
         indices.insert(x);
         indices.insert(y);
 
-        chunks[i] = Chunk(x,y,m);
-        for(int j = 0; j < m; j ++){
-            for(int t = 0; t < m;t++){
-                input.read((char*)&(chunks[i].d_og[j][t] ), 1);
-            }
-        }
-        chunks[k + i] = chunks[i];
-        chunks[k+i].x = chunks[i].y;
-        chunks[k+i].y = chunks[i].x;
+        chunks[i] = Chunk<unsigned char>(x,y,m);
+
+        input.read((char*)&(chunks[i].d), m*m);
+
+
     }
+
+    for (int i = 0; i < chunk_count; ++i) {
+        chunks[chunk_count + i] = chunks[i];
+        chunks[chunk_count+i].x = chunks[i].y;
+        chunks[chunk_count+i].y = chunks[i].x;
+    }
+
+
     sort(chunks.begin(), chunks.end());
 
-    vector<map<int,Chunk>> f_output(indices.size());
+    vector<map<int,Chunk<int>>> f_output(indices.size());
 
 //    int indices_index = 0;
     vector<int> indices_vec(indices.begin(), indices.end());
@@ -123,17 +184,25 @@ int main(int argc, char *argv[])
 
 #pragma omp parallel for  schedule(dynamic, 1)
     for (int ind_index = 0; ind_index < indices_vec.size(); ind_index ++) {
-        int ind = indices_vec[ind_index];
+        int i_index = indices_vec[ind_index];
 
-        auto it_to_row = std::lower_bound(chunks.begin(), chunks.end(), Chunk{ind, 0, {}});
-        for(;it_to_row != chunks.end() && it_to_row->x == ind; it_to_row++){
-            auto it_to_col = std::lower_bound(chunks.begin(), chunks.end(), Chunk{it_to_row->y, 0, {}});
+        auto row_element = std::lower_bound(chunks.begin(), chunks.end(), Chunk<unsigned char>{i_index, 0, {}});
+        for(;row_element != chunks.end() && row_element->x == i_index; row_element++){
+            int j_index = row_element->y;
+            auto col_element = std::lower_bound(chunks.begin(), chunks.end(), Chunk<unsigned char>{j_index, i_index, {}});
 
-            for(;it_to_col!= chunks.end() && it_to_col->x == it_to_row->y && it_to_col->y <= ind; it_to_col++){
-                if(f_output[ind_index].count(it_to_col->y) == 0){
-                    f_output[ind_index][it_to_col->y] = Chunk(ind,it_to_col->y,m);
+            for(; col_element != chunks.end() && col_element->x == j_index; col_element++){
+                int k_index = col_element->y;
+
+
+                if(f_output[ind_index].count(k_index) == 0){
+                    f_output[ind_index][k_index] = Chunk<int>(i_index, k_index, m);
+                    f_output[ind_index][k_index].clr(m);
                 }
-                f_output[ind_index][it_to_col->y] += (*it_to_row)*(*it_to_col);
+
+                mult_add(m,f_output[ind_index][k_index],*row_element,*col_element);
+
+//                f_output[ind_index][col_element->y] += (*row_element) * (*col_element);
 
 
             }
@@ -155,21 +224,30 @@ int main(int argc, char *argv[])
     output.write((char*)&final_chunk_count, 4);
     const int max_val = 0xFF;
 
+
+
     for(const auto int_chunk_map: f_output){
         for (const auto &[k_val, chunk_val] : int_chunk_map) {
             output.write((char*)&(chunk_val.y), 4);
             output.write((char*)&(chunk_val.x), 4);
 
-            for(int j = 0; j < m; j ++){
-                for(int t = 0; t < m;t++){
-                    if((*(chunk_val.d))[j][t] > max_val){
-                        output.write((char*)&(max_val) + 2, 2);
+            for (int i = 0; i < m*m; ++i) {
+                if(chunk_val.d[i] > max_val){
+                        output.write((char*)&(max_val), 2);
                     }else{
-                        output.write((char*)&((*(chunk_val.d))[j][t]) + 2, 2);
+                        output.write((char*)&(chunk_val.d[i]), 2);
                     }
-
-                }
             }
+//            for(int j = 0; j < m; j ++){
+//                for(int t = 0; t < m;t++){
+//                    if((*(chunk_val.d))[j][t] > max_val){
+//                        output.write((char*)&(max_val) + 2, 2);
+//                    }else{
+//                        output.write((char*)&((*(chunk_val.d))[j][t]) + 2, 2);
+//                    }
+//
+//                }
+//            }
         }
     }
 
