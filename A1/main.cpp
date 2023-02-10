e17d8fd196e6f5e656a3b61afc09bd04b90bf7ed
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2023-02-10 15:05:33 +0530

    OK this is promising

--- A1/main.cpp
+++ A1/main.cpp
@@ -184,7 +184,7 @@ int main(int argc, char *argv[])
     vector<int> indices_vec(indices.begin(), indices.end());
 
 
-//#pragma omp parallel for  schedule(dynamic, 1)
+#pragma omp parallel for  schedule(dynamic, 1)
     for (int ind_index = 0; ind_index < indices_vec.size(); ind_index ++) {
         int i_index = indices_vec[ind_index];
 
@@ -211,12 +211,21 @@ int main(int argc, char *argv[])
         }
 
     }
+//    vector<vector<Chunk<unsigned short>>> final_converted_to_print(indices.size());
+//    for (int i = 0; i < f_output.size(); i++) {
+//        final_converted_to_print[i]
+//    }
+
+
+
 
     int final_chunk_count = 0;
 //    #pragma omp parallel for reduction(+:final_chunk_count)
         for (int i = 0; i < f_output.size(); i++) {
             final_chunk_count += f_output[i].size();
         }
+
+
 
     ofstream output(argv[2], ios::binary);
 
