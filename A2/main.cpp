fdc5e4dda3e8386d0737d696dcb8da9eb8c8df8f
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2023-03-04 16:19:33 +0530

    Code for setting tau_hat correctly

--- A2/main.cpp
+++ A2/main.cpp
@@ -10,10 +10,16 @@
 using namespace std;
 
 
+struct query_struct{
+    int u;
+    int v;
+};
+
 struct node_neighbour {
     int node_val;
     int node_deg;
 };
+
 
 struct node {
     int node_val;
@@ -100,15 +106,81 @@ int main(int argc, char *argv[]) {
 
     map<pair<int,int>,int> tau_hat_e;
 
+
+    // Here initialisation by 2, as tau_hat = supp + 2
     for(const auto &node_g: graph){
         for(const auto &node_g_n: node_g.neighbours){
 
-            tau_hat_e[{node_g.node_val,node_g_n.node_val}] = 0;
+            tau_hat_e[{node_g.node_val,node_g_n.node_val}] = 2;
 
         }
     }
 
+
+
+    vector<vector<query_struct>> queries_for_tau(size);
+
     // Here aggregate the total number of queries to be made, maybe have a struct of queries or something
+    for(const auto &node_g: graph){
+        for(const auto &node_g_n_1: node_g.neighbours){
+            for(const auto &node_g_n_2: node_g.neighbours){
+                if(node_g_n_1!=node_g_n_2)
+                    queries_for_tau[node_mapping[node_g_n_1]].push_back({node_g_n_1,node_g_n_2});
+            }
+        }
+    }
+
+
+
+    // Scrap this, gatherv takes care of it, as recv_counts[i] = 0, if nothing to send
+
+    // Remember Last query is {-1,-1} so that everyone sends atleast 1 query
+//    for(auto &ind: queries_for_tau){
+//        ind.push_back({-1,-1});
+//    }
+
+    vector<int> recv_counts(size);
+    for (int i = 0; i < size; ++i) {
+        int send_size = queries_for_tau[i].size();
+        MPI_Gather(&send_size, 1, MPI_INT, recvcounts.data(), 1, MPI_INT, i, MPI_COMM_WORLD);
+    }
+
+    vector<int> displs(size);
+    displs[0] = 0;
+    for (int i = 1; i < size; i++) {
+        displs[i] = displs[i-1] + recvcounts[i-1];
+    }
+
+    int total_queries = displs[size-1] + recvcounts[size-1];
+    vector<query_struct> recv_buf(total_queries);
+
+
+    // Query Struct Datatype
+    MPI_Datatype MPI_QUERY_STRUCT;
+
+    int block_lengths[2] = {1, 1};
+    MPI_Aint displacements[2] = {0, sizeof(int)};
+    MPI_Datatype types[2] = {MPI_INT, MPI_INT};
+
+    MPI_Type_create_struct(2, block_lengths, displacements, types, &MPI_QUERY_STRUCT);
+    MPI_Type_commit(&MPI_QUERY_STRUCT);
+
+
+
+    for (int i = 0; i < size; ++i) {
+        MPI_Gatherv(queries_for_tau[i].data(), queries_for_tau[i].size(), MPI_QUERY_STRUCT, recv_buf.data(), recv_counts.data(), displs.data(), MPI_QUERY_STRUCT, i, MPI_COMM_WORLD);
+    }
+
+
+    // Here we get final tau_hat = supp + 2, as 2 was initialised in the start
+
+    for (const auto& query : recv_buf) {
+        int u = query.u;
+        int v = query.v;
+        if(tau_hat_e.count({u,v}))
+            tau_hat_e[{u,v}]++;
+    }
+
 
     return 0;
 }
